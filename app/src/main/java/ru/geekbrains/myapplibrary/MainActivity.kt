a93b73493384ffb5fd9c113f8f080c6419cc3f8f
Aleksandra <lionessa@km.ru>
2022-11-02 20:18:04 +1000

    вся реализация в MainActivity (без модели MVP)

--- app/src/main/java/ru/geekbrains/myapplibrary/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/myapplibrary/MainActivity.kt
@@ -1,11 +1,58 @@
 package ru.geekbrains.myapplibrary
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import ru.geekbrains.myapplibrary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val counters = mutableListOf(0, 0, 0)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            btnButton1.setOnClickListener {
+                tvText1.text = (++counters[0]).toString()
+                Toast.makeText(this@MainActivity, "Working", Toast.LENGTH_SHORT).show()
+            }
+            btnButton2.setOnClickListener {
+                tvText2.text = (++counters[1]).toString()
+            }
+            btnButton3.setOnClickListener {
+                tvText3.text = (++counters[2]).toString()
+            }
+        }
+    }
+
+    private fun initViews() {
+        with(binding) {
+            tvText1.text = counters[0].toString()
+            tvText2.text = counters[1].toString()
+            tvText3.text = counters[2].toString()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putIntArray("counters", counters.toIntArray())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val array = savedInstanceState.getIntArray("counters")
+        counters.let { list ->
+            list.clear()
+            array?.toList()?.let {
+                list.addAll(it)
+            }
+        }
+        initViews()
     }
 }
